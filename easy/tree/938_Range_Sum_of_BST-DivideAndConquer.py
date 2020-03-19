2cdbf8dae0ccb086af98fcf70f09c23c7f698577
= <=>
2020-03-19 22:02:33 +0800

    update

--- easy/tree/938_Range_Sum_of_BST-DivideAndConquer.py
+++ easy/tree/938_Range_Sum_of_BST-DivideAndConquer.py
@@ -2,6 +2,9 @@ class Solution:
     def rangeSumBST(self, root, L, R):
         if not root:
             return 0
-        l = self.rangeSumBST(root.left, L, R)
-        r = self.rangeSumBST(root.right, L, R)
-        return l + r + (L <= root.val <= R) * root.val
+        if root.val > R:
+            return self.rangeSumBST(root.left, L, R)
+        elif root.val < L:
+            return self.rangeSumBST(root.right, L, R)
+        else :
+            return self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R) + root.val
