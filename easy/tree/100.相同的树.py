3a3f6ab08a797e08c3ac0911130f26563b030e35
= <=>
2020-03-22 17:09:32 +0800

    add 100.相同的树.py

---
+++ easy/tree/100.相同的树.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=100 lang=python3
+#
+# [100] 相同的树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not (p or q):
+            return True
+        if not (p and q):
+            return False
+        if p.val != q.val:
+            return False 
+        return  self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+# @lc code=end
+
