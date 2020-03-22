5c03f659709310eac12a808f612b7d68ffdc366b
= <=>
2020-03-22 17:26:22 +0800

    add 226.翻转二叉树.py

---
+++ easy/tree/226.翻转二叉树.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=226 lang=python3
+#
+# [226] 翻转二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        temp = self.invertTree(root.left)
+        root.left = self.invertTree(root.right)
+        root.right = temp
+        return root
+# @lc code=end
+
