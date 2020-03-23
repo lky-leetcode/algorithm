5d36487fc138f400a900703b53f5b42cb8d29bc6
= <=>
2020-03-23 22:18:46 +0800

    add 96.不同的二叉搜索树.py

---
+++ medium/tree/96.不同的二叉搜索树.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=96 lang=python
+#
+# [96] 不同的二叉搜索树
+#
+
+# @lc code=start
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        G = [0] * (n + 1)
+        G[0], G[1] = 1, 1
+
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                G[i] += G[j - 1] * G[i - j]
+
+        return G[n]
+
+# @lc code=end
+
