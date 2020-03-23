5587242d95cd5a114eb197a282823f6cae5fb8b3
= <=>
2020-03-23 23:42:43 +0800

    add 111.二叉树的最小深度.py

---
+++ easy/tree/111.二叉树的最小深度.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=111 lang=python
+#
+# [111] 二叉树的最小深度
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if not root.right:
+            return self.minDepth(root.left) + 1
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+# @lc code=end
+
