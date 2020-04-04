a86d094d7182176058c4fa1403729cfd9dd134da
lky-leetcode <lkyleetcode@gmail.com>
2020-04-05 02:07:24 +0800

    add 5360.Count Largest Group.py

---
+++ 雙週賽/5360.Count Largest Group.py
@@ -0,0 +1,27 @@
+#Given an integer n. Each number from 1 to n is grouped according to the sum of its digits. 
+#Return how many groups have the largest size.
+#23nd two weekly contest
+#第一題
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        nums = [0] * 100
+        maxbit = 0
+        ans = 0
+        def count(number):
+            result = 0
+            while number != 0:
+                result += number % 10
+                number //= 10
+            return result
+
+        for i in range(1, n + 1):
+            bit = count(i)
+            nums[bit] += 1
+            maxbit = max(nums[bit], maxbit)
+            
+        for i in range(1, 100):
+            if nums[i] == maxbit:
+                ans += 1
+        return ans
+            
