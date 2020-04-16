3b86f16742e6b78f0fa8e9a34e5db6be1745ea5f
lky-leetcode <lkyleetcode@gmail.com>
2020-04-16 23:01:23 +0800

    add 56.合并区间.py

---
+++ easy/tree/542.01-矩阵.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=542 lang=python
+#
+# [542] 01 矩阵
+#
+
+# @lc code=start
+class Solution(object):
+    def updateMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
+# @lc code=end
+
---
+++ medium/array/56.合并区间.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=56 lang=python3
+#
+# [56] 合并区间
+#
+
+# @lc code=start
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
+# @lc code=end
+
