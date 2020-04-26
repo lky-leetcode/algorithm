a020eaaea7784fdd49fff887f7a1c1a5b260698a
lky-leetcode <lkyleetcode@gmail.com>
2020-04-26 21:47:26 +0800

    add 90.子集-ii.py

--- medium/dfs/90.子集-ii.py
+++ medium/dfs/90.子集-ii.py
@@ -11,16 +11,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        def dfs(nums, ans, cur, path):
+        
+        def dfs(nums, ans, cur, path, visited):
             ans.append(path[:])
-            print(f"ans = {ans}")
             for i in range(cur, len(nums)):
+                if i > 0 and not visited[i - 1] and nums[i - 1] == nums[i]:
+                    continue
                 path.append(nums[i])
-                dfs(nums, ans, i + 1, path)
+                visited[i] = True
+                dfs(nums, ans, i + 1, path, visited)
                 path.pop()
+                visited[i] = False
         ans = []
         nums.sort()
-        dfs(nums, ans, 0, [])
+        visited = [False] * len(nums)
+        dfs(nums, ans, 0, [], visited)
         return ans
 # @lc code=end
 
