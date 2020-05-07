cb047fa3acb1f678c95abff891eb4ac5a843ee97
lky-leetcode <lkyleetcode@gmail.com>
2020-05-08 01:02:14 +0800

    update 572.py

--- easy/tree/572.py
+++ easy/tree/572.py
@@ -4,30 +4,18 @@ class TreeNode:
         self.left = None
         self.right = None
 class Solution:
-    is_find = True
-    ans = False
     def isSubtree(self, s, t):
         def isSameTree(s, t):
             if not s and not t:
-                return
+                return True
             if not s or not t:
-                self.is_find = False
-                return
-            if s.val != t.val:
-                self.is_find = False
-                return
-            isSameTree(s.left, t.left)
-            isSameTree(s.right, t.right)
-        def traversal(s, t):
-            if not s:
-                return
-            if s.val == t.val:
-                self.is_find = True
-                isSameTree(s, t)
-                if self.is_find == True:
-                    self.ans = True
-                    return 
-            traversal(s.left, t)
-            traversal(s.right, t)
-        traversal(s, t)
-        return self.ans
+                return False
+            return s.val == t.val and isSameTree(s.left, t.left) and isSameTree(s.right, t.right)
+
+        if not s and not t:
+            return True
+        if not s:
+            return False
+        if isSameTree(s, t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
